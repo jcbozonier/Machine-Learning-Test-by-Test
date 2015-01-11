2b76464106fcd72882414399cb4abedacc8f77f6
Justin Bozonier <darkxanthos@gmail.com>
2015-01-11 13:05:00 -0600

    Randomly chooses previously observed values

--- Chapter 1/NumberGuesser.py
+++ Chapter 1/NumberGuesser.py
@@ -1,7 +1,12 @@
+import random
 class NumberGuesser:
   """Guesses numbers based on the history of your input"""
-  _guessed_number = None
+  _guessed_numbers = None
+  def numbers_were(self, guessed_numbers):
+    self._guessed_numbers = guessed_numbers
   def number_was(self, guessed_number):
-    self._guessed_number = guessed_number
+    self._guessed_numbers = [guessed_number]
   def guess(self):
-    return self._guessed_number
+    if self._guessed_numbers == None:
+      return None
+    return random.choice(self._guessed_numbers)
--- Chapter 1/number_guesser_tests.py
+++ Chapter 1/number_guesser_tests.py
@@ -1,17 +1,32 @@
 from NumberGuesser import NumberGuesser
 
 def given_no_information_when_asked_to_guess_test():
+  # given
   number_guesser = NumberGuesser()
-  result = number_guesser.guess()
-  assert result is None
+  # when
+  guessed_number = number_guesser.guess()
+  # then
+  assert guessed_number is None, 'there should be no guess.'
 
 def given_one_datapoint_when_asked_to_guess_test():
   #given
   number_guesser = NumberGuesser()
-  number_guesser.number_was(5)
-
+  previously_chosen_number = 5
+  number_guesser.number_was(previously_chosen_number)
   #when
-  result = number_guesser.guess()
-
+  guessed_number = number_guesser.guess()
   #then
-  assert type(result) is int, 'then the answer is a number'
+  assert type(guessed_number) is int, 'the answer should be a number'
+  assert guessed_number == previously_chosen_number, 'the answer should be the previously chosen number.'
+
+def given_multiple_datapoints_when_asked_to_guess_many_times_test():
+  #given
+  number_guesser = NumberGuesser()
+  previously_chosen_numbers = [1,2,5]
+  number_guesser.numbers_were(previously_chosen_numbers)
+  #when
+  guessed_numbers = [number_guesser.guess() for i in range(0,100)]
+  #then
+  for guessed_number in guessed_numbers:
+    assert guessed_number in previously_chosen_numbers, 'every guess should be one of the previously chosen numbers'
+  assert len(set(guessed_numbers)) > 1, "It shouldn't always guess the same number."
