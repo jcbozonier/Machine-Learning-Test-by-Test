c4025a4ac2712a6a3b1be6dd3fcec750ffdc376b
Justin Bozonier <darkxanthos@gmail.com>
2015-01-11 17:41:00 -0600

    Completed example.

--- Chapter 1/NumberGuesser.py
+++ Chapter 1/NumberGuesser.py
@@ -1,12 +1,13 @@
 import random
 class NumberGuesser:
   """Guesses numbers based on the history of your input"""
-  _guessed_numbers = None
+  def __init__(self):
+    self._guessed_numbers = []
   def numbers_were(self, guessed_numbers):
-    self._guessed_numbers = guessed_numbers
+    self._guessed_numbers += guessed_numbers
   def number_was(self, guessed_number):
-    self._guessed_numbers = [guessed_number]
+    self._guessed_numbers.append(guessed_number)
   def guess(self):
-    if self._guessed_numbers == None:
+    if self._guessed_numbers == []:
       return None
     return random.choice(self._guessed_numbers)
--- Chapter 1/number_guesser_tests.py
+++ Chapter 1/number_guesser_tests.py
@@ -30,3 +30,40 @@ def given_multiple_datapoints_when_asked_to_guess_many_times_test():
   for guessed_number in guessed_numbers:
     assert guessed_number in previously_chosen_numbers, 'every guess should be one of the previously chosen numbers'
   assert len(set(guessed_numbers)) > 1, "It shouldn't always guess the same number."
+
+def given_a_starting_set_of_observations_followed_by_a_one_off_observation_test():
+    #given
+  number_guesser = NumberGuesser()
+  previously_chosen_numbers = [1,2,5]
+  number_guesser.numbers_were(previously_chosen_numbers)
+  one_off_observation = 0
+  number_guesser.number_was(one_off_observation)
+  #when
+  guessed_numbers = [number_guesser.guess() for i in range(0,100)]
+  #then
+  for guessed_number in guessed_numbers:
+    assert guessed_number in previously_chosen_numbers + [one_off_observation], 'every guess should be one of the previously chosen numbers'
+  assert len(set(guessed_numbers)) > 1, "It shouldn't always guess the same number."
+
+def given_a_one_off_observation_followed_by_a_set_of_observations_test():
+  #given
+  number_guesser = NumberGuesser()
+  print number_guesser._guessed_numbers
+  previously_chosen_numbers = [1,2]
+  one_off_observation = 0
+  number_guesser.number_was(one_off_observation)
+  number_guesser.numbers_were(previously_chosen_numbers)
+
+  all_observations = previously_chosen_numbers + [one_off_observation]
+  #when
+  guessed_numbers = [number_guesser.guess() for i in range(0,100)]
+  #then
+  for guessed_number in guessed_numbers:
+    print guessed_number, all_observations
+    assert guessed_number in all_observations, 'every guess should be one of the previously chosen numbers'
+  assert len(set(guessed_numbers)) == len(all_observations), "It should eventually guess every number at least once."
+
+
+
+
+
