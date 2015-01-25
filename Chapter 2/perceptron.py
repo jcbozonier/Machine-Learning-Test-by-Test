73ce748d77d13bf6b92891a0f5979da9500e15f2
Justin Bozonier <darkxanthos@gmail.com>
2015-01-25 13:52:36 -0600

    next test

--- Chapter 2/perceptron.py
+++ Chapter 2/perceptron.py
@@ -1,3 +1,7 @@
 class Perceptron:
-  def predict(self):
-    return None
+  def __init__(self):
+    self._result = None
+  def train(self, input, correct_output):
+    self._result = correct_output
+  def predict(self, input=None):
+    return self._result
--- Chapter 2/perceptron_tests.py
+++ Chapter 2/perceptron_tests.py
@@ -4,3 +4,9 @@ def no_training_data_supplied_test():
   the_perceptron = perceptron.Perceptron()
   result = the_perceptron.predict()
   assert result is None, 'Should have no result with no training data.'
+
+def simplest_training_set_and_prediction_test():
+  the_perceptron = perceptron.Perceptron()
+  the_perceptron.train([0], 0)
+  result = the_perceptron.predict([0])
+  assert result is 0, 'Should return the only result it learned.'
