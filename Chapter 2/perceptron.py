d33304e0257deb3a6ca5c1f164bd884f8870abb2
Justin Bozonier <darkxanthos@gmail.com>
2015-01-25 16:09:25 -0600

    Reworked tests and refactored

--- Chapter 2/perceptron.py
+++ Chapter 2/perceptron.py
@@ -1,14 +1,14 @@
 class Perceptron:
   def __init__(self):
     self._result = None
+    self._weights = [0]
   def train(self, training_data):
-    weights = [0]
-    for input_data, label in training_data:
-      cross_product = weights[0] * input_data[0]
-      error = label - cross_product
-      weights[0] += error
-    self._weights = weights
+    for i in range(100000):
+      for input_data, label in training_data:
+        cross_product = self._weights[0] * input_data[0]
+        error = label - cross_product
+        self._weights[0] += .1 * error * input_data[0]
   def predict(self, input=None):
     if input is None:
       return None
-    return self._weights[0]*input[0]
+    return self._weights[0]*input[0] > 0
--- Chapter 2/perceptron_tests.py
+++ Chapter 2/perceptron_tests.py
@@ -8,24 +8,36 @@ def no_training_data_supplied_test():
 def simplest_training_set_and_prediction_test():
   the_perceptron = perceptron.Perceptron()
   the_perceptron.train([([0], 0)])
+  print the_perceptron._weights
   result = the_perceptron.predict([0])
-  assert result is 0, 'Should return the only result it learned.'
+  print result
+  assert result == 0.0, 'Should return the only result it learned.'
 
 def predict_what_weve_seen_in_the_past_test():
   the_perceptron = perceptron.Perceptron()
   the_perceptron.train([([0], 0),([1], 1)])
+  print the_perceptron._weights
   result = the_perceptron.predict([0])
+  print result
   assert result == 0
 
   result = the_perceptron.predict([1])
+  print result
   assert result == 1
 
 def seperate_one_dimensional_data_that_generalizes_to_unseen_data_test():
   the_perceptron = perceptron.Perceptron()
-  the_perceptron.train([([0], 0),([-1], 0),([1], 1),([2], 1)])
+  the_perceptron.train([([-2], 0),([1], 1),([-1], 0),([2], 1)])
   print the_perceptron._weights
-  result = the_perceptron.predict([-2])
-  assert result == 0
+
+  result = the_perceptron.predict([-3])
+  print result
+  assert not result, "-3 didn't get classified correctly"
+
+  result = the_perceptron.predict([1])
+  print result
+  assert result, "1 didn't get classified correctly"
 
   result = the_perceptron.predict([3])
-  assert result == 1
+  print result
+  assert result, "3 didn't get classified correctly"
