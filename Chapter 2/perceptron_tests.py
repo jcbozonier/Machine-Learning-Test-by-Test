251a2412347dcb3a1b625497780071c7c60d3e77
Justin Bozonier <darkxanthos@gmail.com>
2015-01-25 18:55:30 -0600

    Cleaned up debugging code in tests.

--- Chapter 2/perceptron_tests.py
+++ Chapter 2/perceptron_tests.py
@@ -8,21 +8,16 @@ def no_training_data_supplied_test():
 def simplest_training_set_and_prediction_test():
   the_perceptron = perceptron.Perceptron()
   the_perceptron.train([([0], 0)])
-  print the_perceptron._weights
   result = the_perceptron.predict([0])
-  print result
   assert result == 0.0, 'Should return the only result it learned.'
 
 def predict_what_weve_seen_in_the_past_test():
   the_perceptron = perceptron.Perceptron()
   the_perceptron.train([([0], 0),([1], 1)])
-  print the_perceptron._weights
   result = the_perceptron.predict([0])
-  print result
   assert result == 0
 
   result = the_perceptron.predict([1])
-  print result
   assert result == 1
 
 def seperate_one_dimensional_data_that_generalizes_to_unseen_data_test():
@@ -31,21 +26,17 @@ def seperate_one_dimensional_data_that_generalizes_to_unseen_data_test():
   print the_perceptron._weights
 
   result = the_perceptron.predict([-3])
-  print result
   assert not result, "-3 didn't get classified correctly"
 
   result = the_perceptron.predict([1])
-  print result
   assert result, "1 didn't get classified correctly"
 
   result = the_perceptron.predict([3])
-  print result
   assert result, "3 didn't get classified correctly"
 
 def learn_AND_with_zero_as_activation_test():
   the_perceptron = perceptron.Perceptron()
   the_perceptron.train([([1,-1], 0), ([1,1], 1), ([-1,1], 0), ([-1,-1], 0)])
-  print the_perceptron._weights
   result = the_perceptron.predict([-1,1])
   assert not result, "should be false"
   result = the_perceptron.predict([1,1])
@@ -54,10 +45,8 @@ def learn_AND_with_zero_as_activation_test():
 def learn_NAND_with_zero_as_activation_test():
   the_perceptron = perceptron.Perceptron()
   the_perceptron.train([([1,-1], 1), ([1,1], 0), ([-1,1], 1), ([-1,-1], 1)])
-  print the_perceptron._weights
   result = the_perceptron.predict([-1,1])
   assert result, "should be true"
   result = the_perceptron.predict([1,1])
-  print result
   assert not result, "should be false"
 
