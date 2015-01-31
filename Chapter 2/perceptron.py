0816cb9d6d51796c042ec8b164c993c17c678499
Justin Bozonier <darkxanthos@gmail.com>
2015-01-31 13:50:27 -0600

    uneccessary variable

--- Chapter 2/perceptron.py
+++ Chapter 2/perceptron.py
@@ -4,15 +4,16 @@ class Perceptron:
     self._bias = bias
     self._training_rate = training_rate
     self._max_iterations = max_iterations
+
   def train(self, training_data):
     self._weights = len(training_data[0][0])*[0]
-    labels = []
     for i in range(self._max_iterations):
       for input_data, label in training_data:
         cross_product = sum(weight_i * input_i for weight_i, input_i in zip(self._weights, input_data))
         error = label - cross_product
         for index, input_i in enumerate(input_data):
           self._weights[index] += self._training_rate * error * input_i
+          
   def predict(self, input=None):
     if input is None:
       return None
