6f7338d58d19f2a4a8cdb731f3ad6cad75fc0f13
Justin Bozonier <darkxanthos@gmail.com>
2015-02-01 23:05:30 -0600

    Added quality test

--- Chapter 2/perceptron_tests.py
+++ Chapter 2/perceptron_tests.py
@@ -1,4 +1,4 @@
-import perceptron
+import perceptron, numpy
 
 def no_training_data_supplied_test():
   the_perceptron = perceptron.Perceptron()
@@ -73,9 +73,27 @@ def learn_AND_with_big_training_rate_test():
   result = the_perceptron.predict([0,1])
   assert not result, "should not predict correctly"
 
-
 def enable_configuring_number_of_iterations_test():
   the_perceptron = perceptron.Perceptron(bias=0.5, max_iterations=1)
   the_perceptron.train([([1,0], 0), ([1,1], 1), ([0,1], 0), ([0,0], 0)])
   result = the_perceptron.predict([1,1])
   assert not result, "should be misclassified"
+
+def when_used_on_a_linearly_seperable_dataset_test():
+  # Given a perceptron trained on a linearly seperable dataset
+  number_correct = 0
+  simulations_to_run = 100
+  datapoints_per_sim = 100
+  for i in range(simulations_to_run):
+    x = numpy.random.uniform(0,10,size=datapoints_per_sim)
+    y = .75*x + 5.0*numpy.random.normal(loc=0.0, scale=2.0, size=datapoints_per_sim)
+    classification = map(lambda t: 1 if t[1] > .75*t[0] else 0, zip(x,y)) 
+    training_dataset = zip(zip(x,y),classification)
+    the_perceptron = perceptron.Perceptron(max_iterations=1000, bias=0.5, training_rate=.001) #, training_rate=.01
+    the_perceptron.train(training_dataset)
+
+    # When predicting
+    test_results = [(the_perceptron.predict(i[0]), i[1]) for i in training_dataset]
+    number_correct += len(filter(lambda x: x[0]==x[1], test_results))
+  print number_correct/(1.0*simulations_to_run*datapoints_per_sim)
+  assert number_correct/(1.0*simulations_to_run*datapoints_per_sim) > 0.90, "Then it should have much better than random performance."
