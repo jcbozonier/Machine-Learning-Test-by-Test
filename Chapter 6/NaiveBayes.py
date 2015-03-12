eea79e31fbd7236e03b82252a1c2b4617bebe2ca
Justin Bozonier <darkxanthos@gmail.com>
2015-03-11 19:54:34 -0500

    updated test to support iterative dev

--- Chapter 6/NaiveBayes.py
+++ Chapter 6/NaiveBayes.py
@@ -6,15 +6,15 @@ class Classifier:
     self._classification_b = None
   def train(self, classification, observation):
     if classification == self._classification_a_label:
-      self._classification_a = observation
+      self._classification_a.append(observation)
     elif classification == self._classification_b_label:
-      self._classification_b = observation
+      self._classification_b.append(observation)
     elif self._classification_a is None:
       self._classification_a_label = classification
-      self._classification_a = observation
+      self._classification_a = [observation]
     elif self._classification_b is None:
       self._classification_b_label = classification
-      self._classification_b = observation
+      self._classification_b = [observation]
   def classify(self, observation):
     if self._classification_a_label is None and self._classification_b_label is None:
       return None
@@ -22,8 +22,7 @@ class Classifier:
       return self._classification_a_label
     else:
       closest_class = self._classification_a_label
-      closest_observation = abs(observation - self._classification_a)
-      if abs(observation - self._classification_b) < closest_observation:
-        print "in class b"
+      closest_observation = abs(observation - self._classification_a[0])
+      if abs(observation - self._classification_b[0]) < closest_observation:
         closest_class = self._classification_b_label
     return closest_class
--- Chapter 6/naive_bayes_tests.py
+++ Chapter 6/naive_bayes_tests.py
@@ -24,11 +24,11 @@ def given_multiple_observations_for_two_classes_test():
   classifier = NaiveBayes.Classifier()
   classifier.train(classification='a class', observation=0.0)
   classifier.train(classification='a class', observation=1.0)
-  classifier.train(classification='a class', observation=0.5)
-  classifier.train(classification='b class', observation=50)
-  classifier.train(classification='b class', observation=15)
+  classifier.train(classification='a class', observation=75.0)
+  classifier.train(classification='b class', observation=25)
+  classifier.train(classification='b class', observation=99)
   classifier.train(classification='b class', observation=100)
-  classification = classifier.classify(observation=23.2)
-  assert classification == 'b class', "Should classify as the best fit class."
-  classification = classifier.classify(observation=2.0)
+  classification = classifier.classify(observation=25)
   assert classification == 'a class', "Should classify as the best fit class."
+  classification = classifier.classify(observation=75.0)
+  assert classification == 'b class', "Should classify as the best fit class."
