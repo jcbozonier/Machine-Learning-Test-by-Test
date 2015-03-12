f6db2bdfd104dbd8e83cf6e921858f916398961c
Justin Bozonier <darkxanthos@gmail.com>
2015-03-11 23:33:06 -0500

    Finished classifier

--- Chapter 6/NaiveBayes.py
+++ Chapter 6/NaiveBayes.py
@@ -2,47 +2,42 @@ import numpy as np
 
 class Classifier:
   def __init__(self):
-    self._classification_a_label = None
-    self._classification_a = None
-    self._classification_b_label = None
-    self._classification_b = None
+    self._classifications = {}
   def train(self, classification, observation):
-    if classification == self._classification_a_label:
-      self._classification_a.append(observation)
-    elif classification == self._classification_b_label:
-      self._classification_b.append(observation)
-    elif self._classification_a is None:
-      self._classification_a_label = classification
-      self._classification_a = [observation]
-    elif self._classification_b is None:
-      self._classification_b_label = classification
-      self._classification_b = [observation]
+    if not classification in self._classifications:
+      self._classifications[classification] = []
+    self._classifications[classification].append(observation)
   def probability_of_data_given_class(self, observation, class_observations):
     mean = np.mean(class_observations)
     variance = np.var(class_observations)
     p_data_given_class = 1/np.sqrt(2*np.pi*variance)*np.exp(-0.5*((observation - mean)**2)/variance)
     return p_data_given_class
-  def _probability_of_each_class_given_data(self, observation, class_a_data, class_b_data):
-    p_class_a = len(class_a_data)/(1.0*(len(class_a_data) + len(class_b_data)))
-    p_class_b = len(class_b_data)/(1.0*(len(class_a_data) + len(class_b_data)))
-    sum_of_probabilities = self.probability_of_data_given_class(observation, class_a_data) * p_class_a \
-                           + self.probability_of_data_given_class(observation, class_b_data)*p_class_b 
-    class_a_probability = self.probability_of_data_given_class(observation, class_a_data)*p_class_a \
-                          / sum_of_probabilities
-    class_b_probability = self.probability_of_data_given_class(observation, class_b_data)*p_class_b \
-                          / sum_of_probabilities
-    return (class_a_probability, class_b_probability)
+  def _probability_of_each_class_given_data(self, observation, classifications):
+    all_observations = 1.0*sum([len(class_values) for class_values in classifications.values()])
+    class_probabilities = { class_label: len(classifications[class_label])/all_observations 
+                            for class_label in classifications.keys()}
+    sum_of_probabilities = sum([self.probability_of_data_given_class(observation, classifications[class_label]) 
+                                * class_probability 
+                                for class_label, class_probability in class_probabilities.items()])
+    probability_class_given_data = {}
+    for class_label, observations in classifications.items():
+      class_probability = self.probability_of_data_given_class(observation, observations) \
+        * class_probabilities[class_label] \
+        / sum_of_probabilities
+      probability_class_given_data[class_label] = class_probability
+    return probability_class_given_data
+  def _any_classes_are_too_small(self, classifications):
+    for item in classifications.values():
+      if len(item) <= 1:
+        return True
+    return False
   def classify(self, observation):
-    if self._classification_a_label is None and self._classification_b_label is None:
+    if len(self._classifications.keys()) == 0:
       return None
-    elif self._classification_b_label is None:
-      return self._classification_a_label
-    elif len(self._classification_a) <= 1 or len(self._classification_b) <= 1:
+    elif len(self._classifications.keys()) == 1:
+      return self._classifications.keys()[0]
+    elif self._any_classes_are_too_small(self._classifications):
       return None
     else:
-      closest_class = self._classification_a_label
-      p_of_A_given_data, p_of_B_given_data = self._probability_of_each_class_given_data(observation, self._classification_a, self._classification_b)
-      if p_of_A_given_data > p_of_B_given_data:
-        return self._classification_a_label
-      else:
-        return self._classification_b_label
+      results = self._probability_of_each_class_given_data(observation, self._classifications)
+      return max(results.items(), key=lambda x: x[1])[0]
