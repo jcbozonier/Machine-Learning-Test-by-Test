afe66ccff19cfc654f12cde01a5322f45c2d7a48
Justin Bozonier <darkxanthos@gmail.com>
2015-04-02 20:04:45 -0500

    Refactored to be a simpler test suite.

--- Chapter 8/choosey.py
+++ Chapter 8/choosey.py
@@ -2,5 +2,22 @@ class NoClassifierOptionsException(Exception):
   pass
 
 class ClassifierChooser:
-    def __init__(self):
-        raise NoClassifierOptionsException()
+    def __init__(self, classifier_options=None):
+        if classifier_options is None:
+            raise NoClassifierOptionsException()
+        else:
+            self._classifier_options = classifier_options
+    def classify(self, input):
+        return self._classifier_options.classify(input)
+
+class AlwaysTrueClassifier:
+    def classify(self, input):
+        return 1
+
+class AlwaysFalseClassifier:
+    def classify(self, input):
+        return 0
+
+class CopyCatClassifier:
+    def classify(self, input):
+        return input
--- Chapter 8/tests/classification_chooser_tests.py
+++ Chapter 8/tests/classification_chooser_tests.py
@@ -4,3 +4,23 @@ from choosey import *
 @nose.tools.raises(NoClassifierOptionsException)
 def given_no_model_options_test():
     classifier_chooser = ClassifierChooser()
+
+def given_a_single_classifier_option_that_does_not_require_training_test():
+    classifier_chooser = ClassifierChooser(classifier_options=CopyCatClassifier())
+    input_value = 42
+    predicted_label = classifier_chooser.classify(input_value)
+    assert predicted_label == input_value, "Should predict input value."
+
+    input_value = 11
+    predicted_label = classifier_chooser.classify(input_value)
+    assert predicted_label == input_value, "Should predict input value."
+
+def given_a_CopyCatClassifier_test():
+    classifier = CopyCatClassifier()
+    input_value = 12.5
+    predicted_label = classifier.classify(input_value)
+    assert predicted_label == input_value, "Should predict the value to be what the input is."
+
+    input_value = 77
+    predicted_label = classifier.classify(input_value)
+    assert predicted_label == input_value, "Should predict the value to be what the input is."
