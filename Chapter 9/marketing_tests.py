0bbd8473b2e50f3a2f4e6a6f16a6a3e40964b010
Justin Bozonier <darkxanthos@gmail.com>
2015-04-12 15:35:22 -0500

    all tests refactored to use new more general class.

--- Chapter 9/marketing_tests.py
+++ Chapter 9/marketing_tests.py
@@ -28,34 +28,43 @@ def given_a_sleeping_dog_test():
     assert ad_name == 'control', "Should let sleeping dogs lie."
 
 def given_a_variant_that_improves_on_probability_of_ordering_over_control_test():
+    customer_segment = ('60626', 'female')
     classification_model = SimplisticClasses.DumbClassifier({
-        ('control', '60626', 'female'): 0.60,
-        ('variant', '60626', 'female'): 0.65,
+        ('control',) + customer_segment: 0.60,
+        ('variant',) + customer_segment: 0.65,
     })
     regression_model = SimplisticClasses.AllCasesHaveSameProfitRegressionModel()
-    customer = ('60626', 'female')
-    ad_name = SimplisticClasses.assign_ad_for(customer, classification_model, regression_model)
+    ad_name = SimplisticClasses.assign_ad_for(customer_segment, classification_model, regression_model)
     assert ad_name == 'variant', "Should choose to advertise"
 
 def given_a_variant_that_does_NOT_improve_on_probability_of_ordering_over_control_test():
-    classification_model = SimplisticClasses.VariantImprovesAndFemaleMoreSoClassifier()
+    customer_segment = ('60626', 'male')
+    classification_model = SimplisticClasses.DumbClassifier({
+        ('control',) + customer_segment: 0.45,
+        ('variant',) + customer_segment: 0.45,
+    })
     regression_model = SimplisticClasses.AllCasesHaveSameProfitRegressionModel()
-    customer = ('60626', 'male')
-    ad_name = SimplisticClasses.assign_ad_for(customer, classification_model, regression_model)
+    ad_name = SimplisticClasses.assign_ad_for(customer_segment, classification_model, regression_model)
     assert ad_name == 'control', "Should choose to NOT advertise"
 
 def given_variant_improves_over_control_but_not_enough_to_warrant_advertising_cost_test():
-    classification_model = SimplisticClasses.VariantImprovesAndFemaleMoreSoClassifier()
+    customer_segment = ('60626', 'female')
+    classification_model = SimplisticClasses.DumbClassifier({
+        ('control',) + customer_segment: 0.60,
+        ('variant',) + customer_segment: 0.65,
+    })
     regression_model = SimplisticClasses.AllCasesHaveSameProfitRegressionModel()
-    customer = ('60626', 'female')
-    ad_name = SimplisticClasses.assign_ad_for(customer, classification_model, regression_model, ad_cost=0.61)
+    ad_name = SimplisticClasses.assign_ad_for(customer_segment, classification_model, regression_model, ad_cost=0.61)
     assert ad_name == 'control', "Should choose to NOT advertise"
 
 def given_variant_improves_over_control_just_enough_to_warrant_advertising_cost_test():
-    classification_model = SimplisticClasses.VariantImprovesAndFemaleMoreSoClassifier()
+    customer_segment = ('60626', 'female')
+    classification_model = SimplisticClasses.DumbClassifier({
+        ('control',) + customer_segment: 0.60,
+        ('variant',) + customer_segment: 0.65,
+    })
     regression_model = SimplisticClasses.AllCasesHaveSameProfitRegressionModel()
-    customer = ('60626', 'female')
-    ad_name = SimplisticClasses.assign_ad_for(customer, classification_model, regression_model, ad_cost=0.60)
+    ad_name = SimplisticClasses.assign_ad_for(customer_segment, classification_model, regression_model, ad_cost=0.60)
     assert ad_name == 'variant', "Should choose to advertise"
 
 def given_probability_to_order_remains_constant_but_expected_profit_increases():
