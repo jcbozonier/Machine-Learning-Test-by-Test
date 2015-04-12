9f6bc1b7470708e5625bed11aa4685e447dfe555
Justin Bozonier <darkxanthos@gmail.com>
2015-04-12 16:59:49 -0500

    Removed unecessary class

--- Chapter 9/SimplisticClasses.py
+++ Chapter 9/SimplisticClasses.py
@@ -10,21 +10,6 @@ class DumbClassifier():
     def predict(self, input):
         return self.probability(input)
 
-class VariantImprovesAndFemaleMoreSoClassifier():
-    def probability(self, input):
-        data = {
-            ('control', '60626', 'female'): 0.60,
-            ('variant', '60626', 'female'): 0.65,
-            ('control', '60626', 'male'): 0.45,
-            ('variant', '60626', 'male'): 0.45,
-            ('control', '60602', 'female'): 0.70,
-            ('variant', '60602', 'female'): 0.70,
-            ('control', '60602', 'male'): 0.50,
-            ('variant', '60602', 'male'): 0.45,
-        }
-        classifier = DumbClassifier(data)
-        return classifier.probability(input)
-
 class AllCasesHaveSameProfitRegressionModel():
     def predict(self, input):
         return 12.25
