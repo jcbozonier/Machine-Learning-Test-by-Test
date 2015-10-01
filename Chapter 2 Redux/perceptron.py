f3f2474dc5d2ed81c7d45c773aa5929da40ba1e7
Justin Bozonier <darkxanthos@gmail.com>
2015-10-01 18:34:56 -0500

    First classification scenario passing.

--- Chapter 2 Redux/perceptron.py
+++ Chapter 2 Redux/perceptron.py
@@ -1,3 +1,11 @@
 class Perceptron:
-  def predict(self):
+  def __init__(self):
+    self._weight_1 = 0.20
+    self._weight_2 = 0.20
+  def train(self, inputs, label):
     pass
+  def predict(self, input):
+    if len(input) == 0:
+      return None
+    return 0 < self._weight_1 * input[0] + self._weight_2 * input[1]
+
--- Chapter 2 Redux/tests.py
+++ Chapter 2 Redux/tests.py
@@ -7,5 +7,19 @@ from perceptron import *
 
 def no_training_data_supplied_test():
   the_perceptron = Perceptron()
-  result = the_perceptron.predict()
+  result = the_perceptron.predict([])
   nt.assert_equal(result, None, 'Should have no result with no training data.')
+
+def train_an_OR_function_test():
+  the_perceptron = Perceptron()
+  the_perceptron.train([
+                           [1,1], 
+                           [0,1], 
+                           [1,0], 
+                           [0,0]
+                         ], 
+                         [1,1,1,0])
+  nt.assert_equal(the_perceptron.predict([1,1]), 1)
+  nt.assert_equal(the_perceptron.predict([1,0]), 1)
+  nt.assert_equal(the_perceptron.predict([0,1]), 1)
+  nt.assert_equal(the_perceptron.predict([0,0]), 0)
