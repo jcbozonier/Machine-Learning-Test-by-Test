4ead499bf1c7be35d841448ac74e415b1f479199
Justin Bozonier <darkxanthos@gmail.com>
2015-10-02 07:46:36 -0500

    Refactored towards a generalized approach

--- Chapter 2 Redux/perceptron.py
+++ Chapter 2 Redux/perceptron.py
@@ -1,15 +1,13 @@
 class Perceptron:
-  def __init__(self):
-    self._weight_1 = 0.431
-    self._weight_2 = 0.02
   def train(self, inputs, labels):
+    self._weights = [0.431, 0.02]
     for _ in range(4):
       for input, label in zip(inputs, labels):
         label_delta = (label - self.predict(input))
-        self._weight_1 = self._weight_1 + .1 * input[0] * label_delta
-        self._weight_2 = self._weight_2 + .1 * input[1] * label_delta
+        for index, x in enumerate(input):
+          self._weights[index] += .1 * x * label_delta
   def predict(self, input):
     if len(input) == 0:
       return None
-    return int(0 < self._weight_1 * input[0] + self._weight_2 * input[1])
+    return int(0 < self._weights[0] * input[0] + self._weights[1] * input[1])
 
